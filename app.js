25c38f9f3eedb21af9bfdf50d8cfb18822a8dbff
Devon Stanton <devon@git.com>
2019-01-30 15:19:17 +0100

    [add] CSS page and express.static

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// use strict;
+'use strict';
 
 const express = require('express');
 const app = express();
@@ -6,6 +6,8 @@ const app = express();
 const port = 3000;
 
 app.set('view engine', 'pug');
+
+app.use(express.static('./'))
 
 app.get('/', (req, res, next) =>{
   res.render('index', {title: 'Media Byte', message: 'Here we go!'})
