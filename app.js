3fe09fac648dc40ee6ff6927564762957a8c9615
Devon Stanton <devon@git.com>
2019-01-30 14:20:52 +0100

    setup initial PUG template

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
-use strict;
+// use strict;
 
 const express = require('express');
-const app = express;
+const app = express();
+
+const port = 3000;
+
+app.set('view engine', 'pug');
+
+app.get('/', (req, res, next) =>{
+  res.render('index', {title: 'Media Byte', message: 'Here we go!'})
+})
+
+app.listen(port, () => {
+  console.log(`Now listening on ${port}`)
+});
